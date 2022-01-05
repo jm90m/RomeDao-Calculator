43220e725ef37e32e6e04cb113dc94dc6dfc860b
JM Myers <jm.myers@everbridge.com>
2022-01-05 17:28:05 -0500

    Update favicon

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,7 @@ import styles from "./layout.module.css";
 import React from "react";
 import i18n from "i18n-js";
 
-export const siteTitle = "ROMEDAO Calculator";
+export const siteTitle = "RomeDao Rewards Calculator";
 
 export default function Layout({
   children,
@@ -15,13 +15,7 @@ export default function Layout({
     <div className="flex flex-col items-center">
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="RomeDAO Calculator" />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+        <meta name="description" content="RomeDAO Rewards Calculator" />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
